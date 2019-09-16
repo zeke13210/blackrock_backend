2972017956e737d424c20fee0fe820204d9d8760
Jet007 <jet007@localhost.localdomain>
2019-09-16 17:40:32 -0400

    Removed

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = "postgresql://<username>:<password>@<host>:<port>/<dbname>"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:TMBlackRock@blackrocktaskmanager.c0uikdugnayd.us-east-1.rds.amazonaws.com:5432/TaskManagerDB"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
