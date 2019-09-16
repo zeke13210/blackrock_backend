e435abf3ee7531ecca81d2c73b5859bbbfddf801
Jet007 <jet007@localhost.localdomain>
2019-09-15 23:22:37 -0400

    Connection to RDS

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
-
 from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+rds = SQLAlchemy(app)
 app.debug = True
+migrate = Migrate(app, rds)
 
+from models import *
 
-
-
-@app.route('/')
-def hello_world():
-    return ('Hey, we have Flask in a Docker container!')
-
-
+@app.route('/', methods=['GET', 'POST'])
+def task():
+    if request.method == 'GET':
+        return ('Hey, we have Flask in a Docker container!!!!')
+    elif request.method == 'POST'
+        # task = Task(task_id="123", name="The is the first task")
+        # rds.session.add(task)
+        # rds.session.commit()
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
