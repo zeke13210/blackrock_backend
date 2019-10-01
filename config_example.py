f25637d66d8da79ec1880a0c5f36b4e9f84e517e
Zachary Farley <zfarley94@gmail.com>
2019-10-01 17:31:08 -0400

    Update config_example.py

--- config_example.py
+++ config_example.py
@@ -2,5 +2,5 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = "enter aws url here"
+    SQLALCHEMY_DATABASE_URI = "enter aws postgres url here"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
