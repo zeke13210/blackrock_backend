f620ce51f42ef37359e0f0b7f72d1e730545faa8
zeke13210 <zfarley94@gmail.com>
2019-10-03 01:42:05 +0000

    update config to match env var

--- config.py
+++ config.py
@@ -3,5 +3,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SQLALCHEMY_DATABASE_URI = str(os.environ['POST_URL'])
-    # SQLALCHEMY_DATABASE_URI = "postgresql://postgres:TMBlackRockBackEnd@blackrocktaskmanager.c0uikdugnayd.us-east-1.rds.amazonaws.com:5432/TaskManagerDB"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
