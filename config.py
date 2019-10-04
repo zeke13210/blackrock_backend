e7580eb84bb1c318bc8a9e4e6502863c6b966315
Jet007 <jet007@localhost.localdomain>
2019-10-03 22:04:58 -0400

    Env Variable for posturl

--- config.py
+++ config.py
@@ -2,6 +2,5 @@ import os
 # basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('POST_URL')
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:TMBlackRockBackEnd@blackrocktaskmanager.c0uikdugnayd.us-east-1.rds.amazonaws.com:5432/TaskManagerDB"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('POST_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
